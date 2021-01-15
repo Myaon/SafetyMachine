ab79eb9b23cc620c69717a9656e003f280dc6589
massiro.myaon@gmail.com <massiro.myaon@gmail.com>
2021-01-15 12:01:59 +0900

    HSV追加

--- image/image_check.py
+++ image/image_check.py
@@ -4,16 +4,17 @@ import cv2
 import csv
 
 def toCSV(state):
-	with open('color.csv', 'a') as f:
+	with open('hsv.csv', 'a') as f:
 		writer = csv.writer(f)
 		writer.writerow(state)
 
 im = cv2.imread("resp.png")
+hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
 
-for x in range(im.shape[0]):
-	for y in range(im.shape[1]):
+for x in range(hsv.shape[0]):
+	for y in range(hsv.shape[1]):
 		#print(im[x,y]) 
-		toCSV(im[x,y])
+		toCSV(hsv[x,y])
 
 
 
