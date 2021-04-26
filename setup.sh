77dab241d5c679360df50fd1182b7e276d840940
Myaon <massiro.myaon@gmail.com>
2021-04-27 07:59:12 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+git clone https://github.com/Myaon/SafetyMachine.git
+sudo apt-get -y install python-opencv
+sudo apt-get -y install python-pexpect
+sudo apt-get -y install libusb-dev libdbus-1-dev libglib2.0-dev 
+sudo apt-get -y install libudev-dev libical-dev libreadline-dev
+sudo pip -y install bluepy
+sudo pip install bluepy
+git clone https://github.com/OpenWonderLabs/python-host.git
